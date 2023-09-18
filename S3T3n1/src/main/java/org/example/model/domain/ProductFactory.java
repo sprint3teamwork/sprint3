25f78d8106f92c96d5d302cfa31572581a2a9ba3
bowserchris <133642309+bowserchris@users.noreply.github.com>
2023-09-18 09:44:42 +0200

    Added ProductFactory class with generic value type
    Created the ProductFactory class in order to give it sole purpose of creating products. Has a generic type value as a parameter to that will be applied to the corresponding Product type (float for Tree, string for Flower or boolean for Decoration)

---
+++ S3T3n1/src/main/java/org/example/model/domain/ProductFactory.java
@@ -0,0 +1,28 @@
+package org.example.model.domain;
+
+public class ProductFactory {
+	
+	/*
+	//abstract factory that processes the input on what object to create, 
+	 * tree, flower or decoration, and then calls the relevant factory specific to the object
+	 * afterwards further options can be implemented on what specific product format
+	 * is required (and further parameters can be included to know which type is 
+	 * needed and implemented)
+	*/
+
+	
+	public <T> Product createProduct(String type, String name, double price, T value) {
+		Product product = null;		
+		switch(type) {
+			case "1", "tree":
+				return new Tree(name, price, (float) value);
+			case "2", "flower":
+				return new Flower(name, price, (String) value);
+			case "3", "decoration":
+				return new Decoration(name, price, (boolean) value);
+			default: 
+				return product;
+		}
+	}
+	
+}
